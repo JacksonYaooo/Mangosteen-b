590375d3ac2354aded4f69b58d4a52b9f14888f2
JacksonYaooo <2594838054@qq.com>
2024-06-26 22:36:22 +0800

    feat: 验证码放到邮件标题里

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
+# encoding: utf-8
 class UserMailer < ApplicationMailer
   def welcome_email(email)
     validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validation_code.code
-    mail(to: email, subject: '山竹记账验证码')
+    mail(to: email, subject: "[#{@code}]山竹记账验证码")
   end
 end
